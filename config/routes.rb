39bf852ca2bde61b64514760bfe18a0d2a751263
Chad Lucas <chad.lucas.19@gmail.com>
2017-09-23 16:25:07 -0700

    add order index page

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,11 @@ Rails.application.routes.draw do
 	namespace :admin do 
 		resources :categories, only: [:index]
 		resources :products, only: [:index]
+    resources :orders
     resources :addresses
 		resources :users do 
       resources :addresses
+      resources :orders
     end
 	end
 
