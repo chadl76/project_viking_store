b381aaaff54f1a685262901c7b5df510853fce34
Chad Lucas <chad.lucas.19@gmail.com>
2017-09-24 11:35:27 -0700

    add order content to order show page

--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -12,15 +12,17 @@ class OrderContent < ApplicationRecord
 
 	def self.get_revenue_by_day(num_day)
 		sql = "SELECT SUM(price * quantity) AS total, orders.checkout_date FROM order_contents JOIN products ON products.id = order_contents.product_id JOIN orders ON orders.id = order_contents.order_id WHERE(orders.checkout_date > current_date - #{num_day}) GROUP BY orders.checkout_date ORDER BY checkout_date DESC"
-
-
-		
 		result =  ActiveRecord::Base.connection.exec_query(sql)
 
 		sum = 0		
 		total = result.rows.each { |x| sum += x[0].to_i.round(2)}
 		sum
 	end
+
+	def value
+		product.price * quantity
+	end
+
 
 
 
