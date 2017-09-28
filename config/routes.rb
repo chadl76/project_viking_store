ef218f191825540c6c1b4314f0f717b8dbecf046
Chad Lucas <chad.lucas.19@gmail.com>
2017-09-27 18:32:38 -0700

    wire up navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/index'
-  end
-
-  namespace :admin do
-    get 'categories/index'
-  end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	get '/dashboard' => 'dashboard#index'
 
-	namespace :admin do 
+  root 'dashboard#index'
+
+	namespace :admin do
+
 		resources :categories, only: [:index, :show]
 		resources :products, only: [:index, :show]
     resources :orders, only: [:index]
